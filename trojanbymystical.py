e8fe949b353f7e50c5cacee1ddd96d28dd92edd1
Mysticteam <86481049+Mysticteam@users.noreply.github.com>
2021-06-25 13:51:58 -0300

    Create trojanbymystical.py

---
+++ trojanbymystical.py
@@ -0,0 +1,22 @@
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/                                                   # Embedded file name: dg                                          import sys, os
+from time import sleep
+os.system('clear')
+logo = '              
+██╗███╗░░░███╗
+██║████╗░████║
+██║██╔████╔██║
+██║██║╚██╔╝██║
+██║██║░╚═╝░██║
+╚═╝╚═╝░░░░░╚═╝
+
+███╗░░░███╗██╗░░░██╗░██████╗████████╗██╗░█████╗░░█████╗░██╗░░░░░
+████╗░████║╚██╗░██╔╝██╔════╝╚══██╔══╝██║██╔══██╗██╔══██╗██║░░░░░
+██╔████╔██║░╚████╔╝░╚█████╗░░░░██║░░░██║██║░░╚═╝███████║██║░░░░░
+██║╚██╔╝██║░░╚██╔╝░░░╚═══██╗░░░██║░░░██║██║░░██╗██╔══██║██║░░░░░
+██║░╚═╝░██║░░░██║░░░██████╔╝░░░██║░░░██║╚█████╔╝██║░░██║███████╗
+╚═╝░░░░░╚═╝░░░╚═╝░░░╚═════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝                                '                                                               print logo                                                        target = raw_input("\x1b[32;1mTarget's Number Phone\x1b[31;1m :\x1b[33;1m ")                                                        sleep(1)                                                          os.system('clear')
+print logo
+print '\x1b[32;1mTarget Found ! '                                 print '\x1b[31;1mStart Attack \x1b[33;1m' + target + '\x1b[31;1m ?'
+lanj = raw_input('\x1b[32;1mTab (y) to Continue\x1b[33;1m = ')
+if lanj == 'y':                                                       while True:
+        print '\x1b[32;1mSuccsses Send Trojan To\x1b[31;1m :\x1b[33;1m ' + target
