cd555b77ad05d544b1ec2516e3f4d6603ed568d9
Mysticteam <86481049+Mysticteam@users.noreply.github.com>
2021-06-25 14:02:04 -0300

    Create Mystictrojanspammer.py

---
+++ Mystictrojanspammer.py
@@ -0,0 +1,9 @@
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/                                                   # Embedded file name: dg                                          import sys, os
+from time import sleep
+os.system('clear')
+logo = '\x1b[31;1m\n _______  ______  _____  _____ _______ __   _\x1b[32;1m     By = 𔑮𝐀࿇𝐌𝐘𝐒𝐓𝐈𝐂𝐀𝐋࿇𝐓𝐄𝐀𝐌𔒙1\x1b[31;1m\n    |    |_____/ |     |   |   |_____| | \\  |\x1b[32;1m     Lets end them mystically\x1b[31;1m\n\x1b[37;1m    |    |    \\_ |_____| __|   |     | |  \\_|\x1b[32;1m     V.1 Beta+\n                                             '                                                               print logo                                                        target = raw_input("\x1b[32;1mTarget's Number Phone\x1b[31;1m :\x1b[33;1m ")                                                        sleep(1)                                                          os.system('clear')
+print logo
+print '\x1b[32;1mNAlvo encontrado ! '                                 print '\x1b[31;1mComeçar o atk \x1b[33;1m' + target + '\x1b[31;1m ?'
+lanj = raw_input('\x1b[32;1mTab (y) to Continue\x1b[33;1m = ')
+if lanj == 'y':                                                       while True:
+        print '\x1b[32;1mTrojan enviado para\x1b[31;1m :\x1b[33;1m ' + target
